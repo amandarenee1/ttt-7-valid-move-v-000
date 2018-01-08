1edb79416d2fb161e1154599b395a3b62f414564
Amanda Wingate <arw0219@gmail.com>
2018-01-08 00:42:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,7 +15,7 @@ def position_taken?(board, position)
     false
   elsif board[position] == nil
     false
-  else board[position]  == "X" || "O"
+  else board[position]  == "X" || board[position] == "O"
     true
   end
 end
