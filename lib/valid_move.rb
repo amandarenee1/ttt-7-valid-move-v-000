d80713115e0bdf16c96fcd6685a13e75896bdf1e
Amanda Wingate <arw0219@gmail.com>
2018-01-08 00:55:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def valid_move?(board, position)
  end
 
 def valid_move?(number_entered, board)
-  number_entered.between?(0, 8) && !(position_taken?(board, number_entered)) 
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
 end
 end
 
